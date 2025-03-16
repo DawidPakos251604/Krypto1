396125f9b17be8abe77aaa72e52358f58155cde5
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-03-16 20:37:28 +0100

    Updated project structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Main {
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {}
 }
